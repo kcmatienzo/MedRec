2edc27bc6d1473be54b1ff8728853ab4f5dfb1c4
Bran <bran.balachandran@gmail.com>
2021-12-03 16:31:48 -0500

    fixed bug: admin search crashed when empty string entered

--- app/src/main/java/com/example/medrecroomdb/activity/AdminSearchUserActivity.java
+++ app/src/main/java/com/example/medrecroomdb/activity/AdminSearchUserActivity.java
@@ -2,6 +2,7 @@ package com.example.medrecroomdb.activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,6 +35,13 @@ public class AdminSearchUserActivity extends AppCompatActivity {
         btnSearchUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String strUserId = editText_searchUserId.getText().toString();
+                Log.d("MYVALUE", "onClick: " + strUserId);
+                if(strUserId.equals("")){
+                    Toast.makeText(getApplicationContext(), "Enter Valid User ID", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
                 Integer userId = Integer.parseInt(editText_searchUserId.getText().toString());
                 try {
